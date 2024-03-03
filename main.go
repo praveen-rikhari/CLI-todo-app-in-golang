865f4cb25b16514f27d26104d677f17771429a2f
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-03 23:56:44 +0530

    Building Root Command

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	fmt.Println("Custom CLI application")
+
+	rootCmd := &cobra.Command{
+		Use:   "greeter",
+		Short: "A basic CLI example",
+		Long:  "A basic CLI example using Cobra",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Welcome to Greeter!")
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+	}
+
 }
